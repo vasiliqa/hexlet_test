acb02cbcc040323234c0ef32c2ffbd11b8ff18f6
Ilya_sche <Ilya_rostov_2004@mail.ru>
2024-12-08 16:53:57 +0300

    Испраление rubocop

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,7 +20,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.html { redirect_to @category }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -32,7 +32,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.html { redirect_to @category }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -45,7 +45,7 @@ class CategoriesController < ApplicationController
     @category.destroy!
 
     respond_to do |format|
-      format.html { redirect_to categories_path, status: :see_other, notice: 'Category was successfully destroyed.' }
+      format.html { redirect_to categories_path, status: :see_other }
       format.json { head :no_content }
     end
   end
